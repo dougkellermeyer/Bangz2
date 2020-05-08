399f65dab81199bab9f35cab60275ac53f520296
Doug Kellermeyer <dkellermeyer@jorsek.com>
2020-05-08 12:29:23 -0400

    added stylists pages and links

--- server.js
+++ server.js
@@ -3,8 +3,38 @@ var app = express();
 var path = require('path');
 
 // viewed at http://localhost:8080
+
+// pages
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/services', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/services.html'));
+});
+
+app.get('/tammy', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/tammy.html'));
+});
+
+app.get('/beth', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/beth.html'));
+});
+
+app.get('/sofia', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/sofia.html'));
+});
+
+app.get('/jackie', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/jackie.html'));
+});
+
+app.get('/shelia', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/shelia.html'));
+});
+
+app.get('/tessa', function(req, res) {
+    res.sendFile(path.join(__dirname + '/pages/tessa.html'));
 });
 
 app.use('/public',express.static(__dirname + '/public'));
