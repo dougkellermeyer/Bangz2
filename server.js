c3b64be0c8e2e033b8057db286039942667e7b58
Doug Kellermeyer <dkellermeyer@jorsek.com>
2020-05-08 12:56:39 -0400

    fixed routing

--- server.js
+++ server.js
@@ -9,31 +9,31 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.get('/services', function(req, res) {
+app.get('/pages/services', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/services.html'));
 });
 
-app.get('/tammy', function(req, res) {
+app.get('/pages/tammy', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/tammy.html'));
 });
 
-app.get('/beth', function(req, res) {
+app.get('/pages/beth', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/beth.html'));
 });
 
-app.get('/sofia', function(req, res) {
+app.get('/pages/sofia', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/sofia.html'));
 });
 
-app.get('/jackie', function(req, res) {
+app.get('/pages/jackie', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/jackie.html'));
 });
 
-app.get('/shelia', function(req, res) {
+app.get('/pages/shelia', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/shelia.html'));
 });
 
-app.get('/tessa', function(req, res) {
+app.get('/pages/tessa', function(req, res) {
     res.sendFile(path.join(__dirname + '/pages/tessa.html'));
 });
 
